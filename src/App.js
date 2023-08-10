e4786ee1b55e8b243f5e704c0b226457457a022e
Taj Brar <taj.brar3047@gmail.com>
2023-08-09 20:09:47 -0500

    Server setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
+import React from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+        .then((res) => res.json())
+        .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
