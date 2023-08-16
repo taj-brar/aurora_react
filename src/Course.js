e2393e88c34056b2ebd9beb8deb963897ba045ae
Taj Brar <taj.brar3047@gmail.com>
2023-08-15 20:50:16 -0500

    Moved extra course info button to side instead of placing on top of course button itself

--- src/Course.js
+++ src/Course.js
@@ -17,20 +17,27 @@ class Course extends React.Component {
         console.log(this.state.course);
         const course = this.state.course;
 
-        return (<button className="course-list-flex-item" onClick={this.state.updateYear}>
-            <p className="course-details" style={{fontSize: "1.1em"}}>{course.getCourseNumber()} {course.getSection()} - {course.getCourseName()}</p>
-            <p className="course-details"></p>
-            <p className="course-details" style={{float: "left"}}>{this.state.course ? this.state.course.getCRN() : "loading..."}</p>
-            <p className="course-details" style={{float: "right"}}>{course.getTerm()} | {course.getCredits()} credits</p>
-            <br/>
-            <p className="course-details">{course.getCampus()} | WALLACE 223</p>
-            <p className="course-details">09:30 am - 10:20 am | MWF | Sep 07, 2022 - Dec 12, 2022</p>
-            <p className="course-details">Heather Matheson</p>
-            <p className="course-details" style={{float: "left"}}>Undergraduate Lecture | Lab required</p>
-            <Popup trigger={<button style={{float: "right"}}>...</button>}>
-                <div></div>
-            </Popup>
-        </button>);
+        return (
+            <div className="course-list-flex-item">
+                <Popup className="extra-course-info" trigger={<button style={{float: "left"}}>&lt;</button>}>
+                    <div></div>
+                </Popup>
+                <button className="course-details" onClick={this.state.updateYear} style={{float: "right"}}>
+                    <p className="course-details"
+                       style={{fontSize: "1.1em"}}>{course.getCourseNumber()} {course.getSection()} - {course.getCourseName()}</p>
+                    <p className="course-details"></p>
+                    <p className="course-details"
+                       style={{float: "left"}}>{this.state.course.getCRN()}</p>
+                    <p className="course-details"
+                       style={{float: "right"}}>{course.getTerm()} | {course.getCredits()} credits</p>
+                    <br/>
+                    <p className="course-details">{course.getCampus()} | WALLACE 223</p>
+                    <p className="course-details">09:30 am - 10:20 am | MWF | Sep 07, 2022 - Dec 12, 2022</p>
+                    <p className="course-details">Heather Matheson</p>
+                    <p className="course-details" style={{float: "left"}}>Undergraduate Lecture | Lab required</p>
+                </button>
+            </div>
+        );
     }
 }
 
